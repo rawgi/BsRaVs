da2772cd6bdfbbd5d2c1153a7d2358a8152dd1cf
rawgi <mail@d-mattes.de>
2015-12-10 10:23:45 +0100

    bs incl phils

--- BS+RA/4/baeckerei.c
+++ BS+RA/4/baeckerei.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+#define BLAECHE 10
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+void* eier(){
+//liefern
+//warten
+	return NULL;
+}
+
+void* mehl(){
+//liefern
+//warten
+	return NULL;
+}
+
+void* milch(){
+//liefern
+//warten
+	return NULL;
+}
+
+void* baecker(){
+//fehlt noch was?
+//ja: jammern
+//nein: backen
+	return NULL;
+}
+
+int main(int argc, char *argv[]){
 	printf("Plaetzchenbaeckerei ...\n\n");
-
+	int i;
+	for(i = 0; i<BLAECHE; i++){
+		
+	}
 	return 0;
 }
